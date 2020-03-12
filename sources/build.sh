142d1e6df7b3c8dcad4253436f944d5a0c4d9e24
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-03-12 15:36:25 +0000

    build.sh: typo

--- sources/build.sh
+++ sources/build.sh
@@ -32,7 +32,7 @@ done
 echo "Instanciate single axis VFs"
 fonttools varLib.instancer -o ../fonts/vf/Montserrat[wght].ttf ../fonts/vf/Montserrat[ALTS,wght].ttf "ALTS=drop"
 fonttools varLib.instancer -o ../fonts/vf/Montserrat-Italic[wght].ttf ../fonts/vf/Montserrat-Italic[ALTS,wght].ttf "ALTS=drop"
-fonttools varLib.instancer -o ../fonts/vf/MontserratAlternates-[wght].ttf ../fonts/vf/Montserrat[ALTS,wght].ttf "ALTS=1"
+fonttools varLib.instancer -o ../fonts/vf/MontserratAlternates[wght].ttf ../fonts/vf/Montserrat[ALTS,wght].ttf "ALTS=1"
 fonttools varLib.instancer -o ../fonts/vf/MontserratAlternates-Italic[wght].ttf ../fonts/vf/Montserrat-Italic[ALTS,wght].ttf "ALTS=1"
 rm ../fonts/vf/Montserrat[ALTS,wght].ttf ../fonts/vf/Montserrat-Italic[ALTS,wght].ttf
 
