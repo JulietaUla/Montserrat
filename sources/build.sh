ec0aec86e54eb42d83bf229a65c55a6a96e13265
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-03-13 21:05:04 +0000

    build.sh: typo

--- sources/build.sh
+++ sources/build.sh
@@ -54,7 +54,7 @@ echo "Fixing VF Meta"
 # done
 statmake --stylespace Montserrat.stylespace --designspace Montserrat.designspace --output-path ../fonts/vf/Montserrat[wght].ttf ../fonts/vf/Montserrat[wght].ttf;
 statmake --stylespace Montserrat-Italic.stylespace --designspace Montserrat-Italic.designspace --output-path ../fonts/vf/Montserrat-Italic[wght].ttf ../fonts/vf/Montserrat-Italic[wght].ttf;
-statmake --stylespace Montserrat.stylespace --designspace Montserrat.designspace --output-path ../fonts/vf/MontserratAlternates-[wght].ttf ../fonts/vf/MontserratAlternates-[wght].ttf;
+statmake --stylespace Montserrat.stylespace --designspace Montserrat.designspace --output-path ../fonts/vf/MontserratAlternates[wght].ttf ../fonts/vf/MontserratAlternates[wght].ttf;
 statmake --stylespace Montserrat-Italic.stylespace --designspace Montserrat-Italic.designspace --output-path ../fonts/vf/MontserratAlternates-Italic[wght].ttf ../fonts/vf/MontserratAlternates-Italic[wght].ttf;
 
 echo "Dropping MVAR"
