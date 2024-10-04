3ffcc51bc9c8a6cd8d2765b08062fc234cc97e8e
Juan del Peral <juan@huertatipografica.com>
2024-10-04 11:31:10 +0200

    regenerated fonts, no more sc

--- alternates.sh
+++ alternates.sh
@@ -3,6 +3,9 @@ echo Freezing and subsetting Alternates
 rm -rf fonts-alternates
 cp -r fonts fonts-alternates
 cd fonts-alternates
+
+shopt -s nullglob # Enable nullglob to avoid errors
+
 for f in variable/*.ttf; do echo && echo Freezing Alternates version for "$f" && pyftfeatfreeze -f 'ss01' -S -U Alternates "$f" "${f//Montserrat/MontserratAlternates}" && rm "$f"; done
 for f in variable/*.ttf; do pyftsubset --recalc-bounds --recalc-average-width --glyph-names --layout-features="*" --name-IDs="*" --unicodes="*" --output-file=$f.temp $f && mv $f.temp $f; done
 
